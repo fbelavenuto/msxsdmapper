68ad92dd644b9dcea9121a3f164a23b9d3db3934
belavenuto@gmail.com <belavenuto@gmail.com@67b5788e-d8ca-28d7-bec9-9937a617ebc5>
2014-11-04 10:49:29 +0000

    Atualizado versão no Nextor para 2.0.4 Realocado alguns diretórios para acomodar versões diferentes do Nextor

--- _make.cmd
+++ _make.cmd
@@ -5,7 +5,6 @@ cd ..
 cd updater
 sjasmplus --lst=sdmupd.lst sdmupd.asm
 cd ..
-Nextor\mknexrom Nextor\Nextor-2.0.3.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
-rem Nextor\mknexrom Nextor\Nextor-2.1-alpha1.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
+Nextor\mknexrom Nextor\Nextor-2.0.4.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
 echo.
 pause
