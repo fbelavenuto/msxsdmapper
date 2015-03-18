620e442dc44674283a04995c4d6497496bead61f
Fabio Belavenuto <belavenuto@gmail.com>
2015-03-18 08:30:20 -0300

    Atualizado versao do Nextor para 2.1 alpha 2

--- _make.cmd
+++ _make.cmd
@@ -5,6 +5,6 @@ cd ..
 cd updater
 sjasmplus --lst=sdmupd.lst sdmupd.asm
 cd ..
-Nextor\mknexrom Nextor\Nextor-2.0.4.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
+Nextor\mknexrom Nextor\Nextor-2.1-alpha2.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
 echo.
 pause
