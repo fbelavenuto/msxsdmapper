458d79bf7c914612c167d79f080bdb0254ff3d42
Fabio Belavenuto <belavenuto@gmail.com>
2019-05-07 10:55:34 -0300

    Updated to Nextor 2.1.0 Beta 2

--- _make.cmd
+++ _make.cmd
@@ -7,7 +7,7 @@ cd updater
 sjasmplus --lst=sdmupd.lst sdmupd.asm
 IF ERRORLEVEL 1 GOTO error
 cd ..
-Nextor\mknexrom Nextor\Nextor-2.1-alpha2.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
+Nextor\mknexrom Nextor\Nextor-2.1.0-beta2.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin
 IF ERRORLEVEL 1 GOTO error
 goto ok
 
