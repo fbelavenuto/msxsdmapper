d903d2dfd27588e749e222216d0d8e1fc252a193
Fabio Belavenuto <belavenuto@gmail.com>
2023-12-06 12:58:35 -0300

    Renaming to uppercase

--- _build.sh
+++ _build.sh
@@ -5,7 +5,7 @@ echo Building Driver
 docker run --rm -it -v $PWD/driver:/src fbelavenuto/8bitcompilers N80 DRIVER.ASM DRIVER.BIN --listing-file DRIVER.LST
 
 echo Building ROM
-docker run --rm -it -v $PWD:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-2.1.0-beta2.base.dat driver/SDMAPPER.ROM /d:driver/driver.bin /m:Nextor/Mapper.ASCII16.bin
+docker run --rm -it -v $PWD:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-2.1.0-beta2.base.dat driver/SDMAPPER.ROM /d:driver/DRIVER.BIN /m:Nextor/Mapper.ASCII16.bin
 
 echo Building Updater
 docker run --rm -it -v $PWD/updater:/src fbelavenuto/8bitcompilers N80 SDMUPD.ASM SDMUPD.COM --listing-file SDMUPD.LST
